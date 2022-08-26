6e0997ab03197f71573594fffea07ab338477027
arielahr <arielah1997@gmail.com>
2022-08-26 23:39:00 +0300

    fix

--- src/components/stages/PreviewForum.js
+++ src/components/stages/PreviewForum.js
@@ -37,7 +37,7 @@ const PreviewForum = ({ forum }) => {
                     </Link>{' '}
                     |{' '}
                     <Link
-                        href={forum.links.new}
+                        href={forum.links.newThread}
                         textDecoration={'underline'}
                         target={'_blank'}
                     >
